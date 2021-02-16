3a2172fbd19d8e261bf3290530d01c0b6cf2c3ea
Felipe Layos <jfelipelayos@gmail.com>
2021-02-16 14:04:33 -0500

    Prime numbers & timing execution

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+function countPrimeNumbers() {
+    let count = 0;
+    let sw;
+    for (let i = 2; i <= 100; i++) {
+        sw = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                sw = false;
+                break;
+            }
+        }
+        if (sw) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+t1 = performance.now();
+countPrimeNumbers();
+t2 = performance.now();
+console.log(
+    `Execution time of printing countPrimeNumbers was ${t2 - t1} milliseconds.`
+);
